8a63aca4c21271c133db60572756b002705ee3e2
JonathanSecondGithub <kariuki.jonathan@students.jkuat.ac.ke>
2023-09-26 06:27:04 +0300

    config files

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
-	  for (let idx in array) {
-		      let value = array[idx];
-		      array[idx] = appendString + value;
-		    }
-
-	  return array;
+	for (const idx of array) {
+		arrayEnd.push(`${appendString}${idx}`);
+	}
+	return arrayEnd;
 }
--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,3 +1,3 @@
-export default function returnHowManyArguments() {
-	return returnHowManyArguments.length
+export default function returnHowManyArguments(...theArgs) {
+	return theArgs.length
 }
